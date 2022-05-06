d19da06a62807d05ac300b2dbbd9b7562850df87
ravipatel <dev.rjpatel2290@gmail.com>
2022-05-06 23:52:22 +0530

    Send email using GMail APIs - OAuth

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# send-email-using-gmail-api-oauth
-Send email using GMail APIs - OAuth
+# Send Email using GMail APIs - OAuth
+
+## Setup Google Developer Console Project:
+
+### Step 1: Go to Google Developers Console (https://console.cloud.google.com/). 
+### Step 2: Choose 'Select a project' and make a new one. Name it and hit the 'Create' button.
+### Step 3: On the left bar, choose 'Library' and move to the API Library page. Find Gmail API and click on it. Enable the API for the chosen project.
+### Step 4: Once the API is enabled, you will be taken to a Credentials dashboard. There, select the 'OAuth Client ID' from the Create Credentials dropdown list. 
+### Step 5: Download OAuth Client credential JSON file and save it to your project directory.
+### Step 6: Then you’ll see the 'Configure consent' button. By clicking, you'll get to the page where you can simply enter the name of your application and specify the authorized domains. Please fill in the other fields if you wish.
+### Step 6: Click 'Save' and choose your app type (Web App, Android, Chrome App, iOS, or other). After that, name your OAuth Client ID. Also, enter JavaScript sources and redirect domains to use with requests from the browser or web server. Click 'Create' to complete.
+
+
+## Setup Google Client Library for PHP
+
+### Run below command
+> composer require google/apiclient:"^2.0"
+
+## Get Authorization from Gmail
+
+### Run below command to generate token.json
+> php index.php
+
+
+To generate token.json file, run this file in your Commad Line. i.e. php index.php
+It will generate one link which need to be run in browser.
+After authenticating to your Google App, it will redirect to specified redirect URL.
+Copy "code" parameter value from url and paste it to commad line. i.e. Enter Verification Code: XXXX
+Now check token.json in you directory. 
+Comment below IF block once successfully token generated.
+
+```
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+```
---
+++ index.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * 
+ * Author:    Ravi Patel
+ * Created:   06-05-2022
+ * Version:   1.0
+ **/
+
+require __DIR__ . '/vendor/autoload.php';
+
+/*
+ * To generate token.json file, run this file in your Commad Line. i.e. php index.php
+ * It will generate one link which need to be run in browser.
+ * After authenticating to your Google App, it will redirect to specified redirect URL.
+ * Copy "code" parameter value from url and paste it to commad line. i.e. Enter Verification Code: XXXX
+ * Now check token.json in you directory. 
+ * Comment below IF block once successfully token generated.
+ */
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Gmail API PHP Quickstart');
+    $client->setScopes(["https://mail.google.com/","https://www.googleapis.com/auth/gmail.send"]);
+    $client->setAuthConfig('credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+/**
+ * Get the API client and construct the service object.
+ */ 
+$client     = getClient();
+$service    = new Google_Service_Gmail($client);
+
+/**
+ * @param $sender      array  sender name and email address
+ * @param $recipient   array  recipient name and email address
+ * @param $subject     string email subject
+ * @param $messageText string email text
+ * @return Google_Service_Gmail_Message
+ */
+function createMessage($sender, $recipient, $subject, $messageText) {
+    $rawMessageString = "From: ".$sender["name"]." <".$sender["email"].">\r\n";
+    $rawMessageString .= "To: ".$recipient["name"]." <".$recipient["email"].">\r\n";
+    $rawMessageString .= 'Subject: =?utf-8?B?' . base64_encode($subject) . "?=\r\n";
+    $rawMessageString .= "MIME-Version: 1.0\r\n";
+    $rawMessageString .= "Content-Type: text/html; charset=utf-8\r\n";
+    $rawMessageString .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
+    $rawMessageString .= $messageText;
+
+    $mime = rtrim(strtr(base64_encode($rawMessageString), '+/', '-_'), '=');
+    
+    $msg = new Google_Service_Gmail_Message();
+    $msg->setRaw($mime);
+    return $msg;
+}
+
+/**
+ * @param $service     object  service object
+ * @return Google_Users_Gmail_Lebels
+ */
+function getUserAccountLabel($service) {
+    $user = 'me';
+    $results = $service->users_labels->listUsersLabels($user);
+
+    if (count($results->getLabels()) == 0) {
+      print "No labels found.\n";
+    } else {
+      print "Labels:\n";
+      foreach ($results->getLabels() as $label) {
+        printf("- %s\n", $label->getName());
+      }
+    }
+}
+
+/**
+ * @param $service   object  service object
+ * @param $msg       string  raw message text
+ * @return Google_Users_Gmail_Lebels
+ */
+function sendEmail($service,$msg) {
+    try {
+        $response = $service->users_messages->send("me", $msg);
+        return $response;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
+
+$subject            = "Your Subject";
+$messageText        = "Your message body";
+$sender             = array("name" => "Sender Name", "email" => "sender_email@gmail.com");
+$recipient          = array("name" => "Recipient Name", "email" => "recipient_email@gmail.com");
+
+/**
+ * First create raw message including "sender" and "recipient" info
+ */
+$msg = createMessage($sender, $recipient, $subject, $messageText);
+
+/**
+ * To send email, pass "service object" and "raw message" parameter to sendEmail function
+ */
+$response = sendEmail($service,$msg);
+
+print_r($response);
+?>
